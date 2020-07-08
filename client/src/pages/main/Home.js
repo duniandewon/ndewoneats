40a67f63dbfc0f6e9af7ccbd09e5b22c0c592660
Mohammad Abolfaz <duniandewon@gmail.com>
2020-07-08 07:00:48 +0700

    Create home page
    - load user in home page
    - Create product menus

--- client/src/pages/main/Home.js
+++ client/src/pages/main/Home.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useEffect, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loadUser } from '../../redux/actions/authActions';
+import { Link } from 'react-router-dom';
 
-const Home = () => {
-  return <h1>Home Page</h1>;
+const Home = ({ loadUser }) => {
+  const links = [
+    'promo',
+    'pizza',
+    'pasta',
+    'chicken',
+    'rice',
+    'snacks',
+    'desserts',
+    'drinks',
+  ];
+
+  useEffect(() => {
+    loadUser();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <Fragment>
+      <h1 className='section-title'>Our menu</h1>
+      <div className='menus'>
+        {links.map((link) => (
+          <Link to={`/menu/${link}`} className='menu'>
+            <div className='card'>
+              <img
+                src='https://images.pexels.com/photos/2983101/pexels-photo-2983101.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+                alt={link}
+                className='card__image'
+              />
+              <div className='card__body'>
+                <h3 className='card__title'>{link}</h3>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
-export default Home;
+Home.propTypes = {
+  loadUser: PropTypes.func.isRequired,
+};
+
+export default connect(null, { loadUser })(Home);
