3a4436707b8ed68c235c10f8f59eb068b3bf4e0c
Mohammad Abolfaz <duniandewon@gmail.com>
2020-07-10 14:21:21 +0700

    Create dropdown menu

---
+++ client/src/components/DropdownMenu.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useContext, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { uiContext } from '../context/ui/UiState';
+
+const DropdownMenu = (props) => {
+  const { links, history } = props;
+  const [Dropdown, setDropdown] = useState('');
+
+  const { toggleUi, dropdown } = useContext(uiContext);
+
+  useEffect(() => {
+    const menu = history.location.pathname;
+    const current = menu.split('/')[2];
+    setDropdown(current);
+    // eslint-disable-next-line
+  }, [props.history.location]);
+
+  return (
+    <div className='dropdown hide-on-desktop'>
+      <button
+        className='dropdown__trigger'
+        onClick={() => toggleUi('dropdown')}
+      >
+        {Dropdown}
+      </button>
+      <Fragment>
+        {dropdown ? (
+          <i className='material-icons' onClick={() => toggleUi('dropdown')}>
+            arrow_drop_up
+          </i>
+        ) : (
+          <i className='material-icons' onClick={() => toggleUi('dropdown')}>
+            arrow_drop_down
+          </i>
+        )}
+      </Fragment>
+      <div
+        className={`backdrop ${dropdown && 'active'}`}
+        onClick={() => toggleUi('dropdown')}
+      />
+      <div className={`dropdown__content ${dropdown && 'active'}`}>
+        {links.map((link) => (
+          <div key={link.text} className='sidenav__item'>
+            <Link
+              to={link.to}
+              className='sidenav__link'
+              onClick={() => {
+                toggleUi('dropdown');
+                setDropdown(link.text);
+              }}
+            >
+              {link.text}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DropdownMenu;
--- client/src/pages/main/Menu.js
+++ client/src/pages/main/Menu.js
@@ -1,15 +1,16 @@
 import React, { useEffect, Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import SideNav from '../../layout/SideNav';
-import SearchBox from '../../components/SearchBox';
-
 import Promo from '../products/Promo';
 import Pizza from '../products/Pizza';
 import Pasta from '../products/Pasta';
+
+import SideNav from '../../layout/SideNav';
+import SearchBox from '../../components/SearchBox';
+import DropdownMenu from '../../components/DropdownMenu';
 import ProductDetail from '../../components/ProductDetail';
 
-const Menu = () => {
+const Menu = (props) => {
   const links = [
     { to: '/menu/promo', text: 'promo' },
     { to: '/menu/pizza', text: 'pizza' },
@@ -28,6 +29,7 @@ const Menu = () => {
         <SideNav links={links}>
           <SearchBox />
         </SideNav>
+        <DropdownMenu links={links} history={props.history} />
         <Switch>
           <Route path='/menu/promo' component={Promo} />
           <Route path='/menu/pizza' component={Pizza} />
