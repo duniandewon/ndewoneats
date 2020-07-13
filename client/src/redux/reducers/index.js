fcaf69c08545c60490f16a3d37a972b30f826914
Mohammad Abolfaz <duniandewon@gmail.com>
2020-07-13 20:29:10 +0700

    Include orderReducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -3,10 +3,11 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import productReducer from './productReducer';
 import cartReducer from './cartReducer';
-// order reducer
+import orderReducer from './orderReducer';
 
 export default combineReducers({
   auth: authReducer,
   products: productReducer,
   cart: cartReducer,
+  orders: orderReducer,
 });
