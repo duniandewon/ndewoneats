7f24c1bc8ec9ee9c92eb01c6eb8a28a89775b597
Mohammad Abolfaz <duniandewon@gmail.com>
2020-07-19 21:22:25 +0700

    Serve Static assets in production

--- server.js
+++ server.js
@@ -61,6 +61,13 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/products', require('./routes/products'));
 app.use('/api/orders', require('./routes/orders'));
 
+// Serve Static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
 /**
  * ----------------- SERVER ------------------------
  */
