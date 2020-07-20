b2ddc1bf8e50bd72f03c15d50aa61fc63ef54f52
Mohammad Abolfaz <duniandewon@gmail.com>
2020-07-20 13:09:42 +0700

    Set static assets

--- server.js
+++ server.js
@@ -66,6 +66,8 @@ app.use('/api/orders', require('./routes/orders'));
 
 // Serve Static assets in production
 if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
